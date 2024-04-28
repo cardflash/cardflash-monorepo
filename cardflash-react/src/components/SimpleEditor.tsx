9b8445a5ae360a42751f3365044c4b04563e1533
aarkue <mail@aarkue.eu>
2024-04-28 23:26:18 +0200

    Revampt editor (allow readonly + add SourceLink)
    Co-authored-by: alinaatt <alinaatt@users.noreply.github.com>

--- cardflash-react/src/components/SimpleEditor.tsx
+++ cardflash-react/src/components/SimpleEditor.tsx
@@ -7,10 +7,17 @@ import { Editor } from "@tiptap/core";
 import StarterKit from "@tiptap/starter-kit";
 import MenuBar from "./simple-editor/MenuBar";
 import { MathExtension } from "@aarkue/tiptap-math-extension";
+import Link from "@tiptap/extension-link";
 import "katex/dist/katex.min.css";
+import { SourceLink, SourceLinkAttributes } from "./simple-editor/SourceLink";
+import clsx from "clsx";
+import { useEffect } from "react";
 
 export default function SimpleEditor(props: {
   editorCallback?: (editor: Editor) => unknown;
+  visitSource?: (source: SourceLinkAttributes) => unknown;
+  editable?: boolean;
+  content?: string;
 }) {
   const editor = useEditor({
     onCreate: (args) => {
@@ -18,9 +25,20 @@ export default function SimpleEditor(props: {
         props.editorCallback(args.editor);
       }
     },
+    editable: props.editable ?? true,
+    content: props.content,
     extensions: [
       StarterKit.configure(),
       Highlight,
+      Link.configure({
+        HTMLAttributes: {
+          rel: "",
+          target: null,
+        },
+      }),
+      SourceLink.configure({
+        visitSource: props.visitSource,
+      }),
       TaskList,
       TaskItem,
       Image.configure({
@@ -34,10 +52,22 @@ export default function SimpleEditor(props: {
     ],
   });
 
+  useEffect(() => {
+    if (!props.editable && props.content) {
+      editor?.commands.setContent(props.content);
+    }
+  }, [editor?.commands, props.content, props.editable]);
+
   return (
     <div className="editor">
-      {editor && <MenuBar editor={editor} />}
-      <EditorContent className="editor-prose" editor={editor} />
+      {editor && props.editable !== false && <MenuBar editor={editor} />}
+      <EditorContent
+        className={clsx(
+          "editor-prose",
+          props.editable !== false && "editor-editable",
+        )}
+        editor={editor}
+      />
     </div>
   );
 }

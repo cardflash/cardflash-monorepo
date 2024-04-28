367056ccffe7d12d2d1e459b98d7a76322ebea98
aarkue <mail@aarkue.eu>
2024-04-28 23:27:22 +0200

    Cleanup

--- cardflash-react/src/App.tsx
+++ cardflash-react/src/App.tsx
@@ -42,7 +42,6 @@ export default function App() {
       <TypesafeI18n locale={locale}>
         <QueryClientProvider client={queryClient}>
           <RouterProvider router={router} />
-          {/* <TanStackRouterDevtools position="top-right" /> */}
         </QueryClientProvider>
       </TypesafeI18n>
     </TooltipProvider>
